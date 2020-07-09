81706d3567c4c070b54cb4300c34e63952a292c4
angithadas <angitha@qburst.com>
2020-07-09 17:33:12 +0530

    Optimised import

--- app/src/main/java/com/example/jetpack_paging3/ui/fragments/DetailFragment.kt
+++ app/src/main/java/com/example/jetpack_paging3/ui/fragments/DetailFragment.kt
@@ -1,14 +1,12 @@
 package com.example.jetpack_paging3.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.example.jetpack_paging3.R
 import com.example.jetpack_paging3.databinding.FragmentDetailBinding
-import com.example.jetpack_paging3.databinding.FragmentListingBinding
 
 /**
  * A simple [Fragment] subclass displays detail screen.
